0cd642cdeb1ad3ad7bc4201ea0e544c9808483d2
dsansnuoc - David Sans Naudó <dsans@wordplex.es>
2023-05-25 06:30:07 +0200

    PEC2

--- readme.md
+++ readme.md
@@ -2,5 +2,3 @@ Executar:
 
 npm start
 
-
-Compilar JavaScript
--- src/js/index.compiled.js
+++ src/js/index.compiled.js
@@ -22,4 +22,34 @@ function main() {
         }
         menuBar = !menuBar;
     });
+    transacciones();
 };
+
+function transacciones() {
+
+    const imagenes = document.querySelectorAll('.imagen');
+
+    // Establece el índice de la imagen activa
+    let indiceImagenActiva = 0;
+    
+    function cambiarImagen() {
+// if (imagenes!=undefined) {
+
+      // Remueve la clase "active" de la imagen actual
+      imagenes[indiceImagenActiva].classList.remove('active');
+    
+      // Incrementa el índice de la imagen activa
+      indiceImagenActiva = (indiceImagenActiva + 1) % imagenes.length;
+    
+      // Agrega la clase "active" a la nueva imagen activa
+      imagenes[indiceImagenActiva].classList.add('active');
+    
+      // Establece un tiempo de espera antes de cambiar a la siguiente imagen
+      setTimeout(cambiarImagen, 3000); // Cambiar imagen cada 3 segundos (3000 milisegundos)
+    }
+    
+    // Inicia la transición de imágenes
+    setTimeout(cambiarImagen, 3000);
+//}
+}
+
